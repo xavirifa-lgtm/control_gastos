4b1ac6a3f78f23e01c07d340c78a4bac0ba75ab7
xavirifa-lgtm <xavi.rifa@googlemail.com>
2026-04-02 09:14:25 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -7,12 +7,6 @@ const CACHE_NAME = 'gastos-pwa-v1.0.1';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
-  './style.css',
-  './main.js',
-  './scanner.js',
-  './dashboard.js',
-  './prices.js',
-  './db.js',
   './manifest.json',
   './pwa_icon_512.png',
   'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap',
